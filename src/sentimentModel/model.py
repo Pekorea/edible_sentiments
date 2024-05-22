0a81b0da813d410f29912fd4b737a89936163db3
Pekorea <okatahi1018@gmail.com>
2024-05-22 16:52:43 +0100

    hugging face added

--- src/sentimentModel/model.py
+++ src/sentimentModel/model.py
@@ -1,32 +1,23 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
 from transformers import pipeline
 
 sentiment_pipeline = pipeline("sentiment-analysis")
 
-app = Flask(__name__)
-CORS(app)
+def get_sentiment(text):
+    result = sentiment_pipeline(text)[0]
+    sentiment = result['label']
+    score = result['score']
+    
+   # if sentiment == 'POSITIVE':
+    #    emoji = '😊'
+    #elif sentiment == 'NEGATIVE':
+     #   emoji = '😡'
+    #else:
+     #   emoji = '😑'
 
-@app.route('/analyze', methods=['POST'])
-def analyze():
-    data = request.json
-    text = data.get('text')
-    if not text:
-        return jsonify({'error': 'No text provided'}), 400
-
-    result = sentiment_pipeline(text)
-    sentiment = result[0]['label']
-    score = result[0]['score']
-
-    # Convert sentiment label to an emoji
-    if sentiment == 'POSITIVE':
-        emoji = '😊'
-    elif sentiment == 'NEGATIVE':
-        emoji = '😡'
-    else:
-        emoji = '😑'
-
-    return jsonify(emoji=emoji, sentiment=score)
+    return sentiment, score
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    while True:
+        text = input('Enter text: ')
+        emoji, score = get_sentiment(text)
+        print(f'Sentiment: {sentiment} (Score: {score})')
